10d0c331909a637213dfd66b74f1a34b5f8f57b9
BaraQabaja <bara.qabaja@gmail.com>
2023-11-07 12:43:06 +0200

    Update controllers folder (authController)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,95 +1,447 @@
-const config = require('../config');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const RefreshToken = require('../models/RefreshToken');
+const crypto = require("crypto");
+const config = require("../config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const RefreshToken = require("../models/RefreshToken");
+const { Op } = require("sequelize");
 
+//send email function
+const sendEmail = require("../utils/sendEmail");
+
+// create token functions
+const createToken = require("../utils/createToken");
+
+//httpStatus key words
+const httpStatusText = require("../utils/httpStatusText");
+
+//models
+const User = require("../models/User");
+
+
+
+// @desc    Protect logic (verify token issue) authinticaion middleware
+exports.protect = async (req, res, next) => {
+  //Check if token sent with req or not
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else {
+    res.send("please send a token to verify you");
+  }
+  if (!token) {
+    return next(
+      res.send("You are not login, Please login to get access this route")
+    );
+  }
+
+  // 2) Verify token (no change happens, expired token)
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (error, decoded) => {
+    if (error) {
+      res.send("Token is invalid");
+    }
+  });
+  // If verification succeeds
+  decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  // 3) Check if user exists
+  const currentUser = await User.findByPk(decoded.userId);
+  if (!currentUser) {
+    res.send("The user that belong to this token does no longer exist");
+  }
+
+  if (currentUser.logoutAt) {
+    const logoutAt = parseInt(currentUser.logoutAt.getTime() / 1000, 10);
+    if (logoutAt > decoded.iat) {
+      return res.send("you recently logout. please login again ..");
+    }
+  }
+  req.user = currentUser;
+  next();
+};
+
+// @desc    Logout logic 
+// @route   POST /auth/logout
+// @access  Public
 exports.logoutUser = async (req, res) => {
-    // const refreshToken = req.body.token;
-
-    // if (!refreshToken) return res.status(400).json({ error: "No refresh token provided" });
-
-    try {
-        const authHeader = req.headers['authorization'];
-        const accessToken = authHeader && authHeader.split(' ')[1];
-
-        
-        // await TokenBlacklist.create({ token: token }); // store blacklisted token to the database
-        //  jwt.verify(accessToken, config.auth.accessToken,  (err, user) => {
-        //if (err) return res.sendStatus(403);
-
-        //   const result = await RefreshToken.destroy({ where: { token: refreshToken } });
-
-        //   if (result === 0) {
-        //        return res.status(404).json({ error: "Refresh token not found" });
-        //   }
-       
-        res.status(200).json({ message: "Successfully logged out" });
-        //  });
-    } catch (error) {
-        res.status(500).json({ error: "Failed to log out" });
-    }
+    User.findByPk(req.user.email)
+    .then(async (user) => {
+      if (user) {
+        user.logoutAt = Date.now(); //change the logoutAt attripute to be Date.now() so that allow you to check the time that the user logedout so if he entered to any unotharized page and send a request or do any thing he will be forworded to login page, i will use this property in protect function
+        return await user.save();
+      }
+    })
+    .then((result) => {
+      if (result) {
+        res.send("you logout");
+      } else res.send("something went wrong, please login again");
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
 };
 
+// @desc    Register user logic, create new user
+// @route   POST /auth/register
+// @access  Public
 exports.registerUser = async (req, res) => {
-    // Registration logic goes here
-    const email = req.body.email;
-    try {
-        // Check if user already exists
-        const existingUser = await User.findOne({ where: { email } });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-        // Create a new user
-        const user = await User.create({ ...req.body });
-        // Respond with the created user
-        return res.status(201).json(user);
+  //12 is the number of rounds of hashing. so the password hashing process is more computationally expensive and therefore more secure against brute-force attacks.
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const email = req.body.email;
+  const username = req.body.username;
+  console.log("signup", email, password);
+  //check if the password and confirm password is matched or not
+  if (password !== confirmPassword) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "password and confirmation password must be the same" },
+    });
+  }
 
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
+  //check if the user email is in the db or not
+  const isExistEmail = await User.findOne({
+    where: { email },
+  });
+  // const isVerified=isExistEmail.verifiedEmail
+  if (isExistEmail && isExistEmail.verifiedEmail) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "email already registered" },
+    });
+  }
+  if (isExistEmail && !isExistEmail.verifiedEmail) {
+    await User.destroy({
+      where: { email: email },
+    });
+  }
+
+  //check if the user email not valid or not valid (email syntax check)
+  const isValid = await validator.isEmail(email);
+  if (!isValid) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "you entered invalid email" },
+    });
+  }
+
+  //check password length
+  if (password.length < 8) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "password length less than 8 characters" },
+    });
+  }
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashPassword) => {
+      if (hashPassword) {
+        //create new user with the given data
+        User.create({
+          email: email,
+          username: username,
+          password: hashPassword,
+          verifiedEmail: false,
+        })
+          .then(async (user) => {
+            if (user) {
+              //verify
+              const randomCode = crypto.randomBytes(20).toString("hex");
+
+              // Save the token to the user's record in the database after registration
+              // This assumes you have a User model and a field for verificationToken
+              const token = randomCode;
+              user.emailVerificationToken = token;
+              await user.save();
+              try {
+                const verificationLink = `http://localhost:3000/verify/${token}`;
+
+                await sendEmail({
+                  email: user.email,
+                  subject: "Account Verification",
+                  content: `Click <a href="${verificationLink}">here</a> to verify your account.`,
+                });
+              } catch (error) {
+                user.emailVerificationToken = null;
+
+                await user.save();
+
+                return res.json({
+                  status: httpStatusText.ERROR,
+                  data: { title: "email not sent", message: error.message },
+                });
+              }
+
+              return res.status(200).json({
+                status: httpStatusText.SUCCESS,
+                data: { title: "Reset code sent to email" },
+              });
+
+              // return res.status(200).json({
+              //   status: httpStatusText.SUCCESS,
+              //   data: { title: "registration successful" },
+              // });
+            } else {
+              return res.status(400).json({
+                status: httpStatusText.FAIL,
+                data: { title: "registration failed" },
+              });
+            }
+          })
+          .catch((err) => {
+            res.status(500).send(err || "Something went wrong in the db");
+          });
+      } else {
+        res.send("something went wrong in returning hash password");
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err || "Something went wrong");
+    });
 };
 
-
-
-
+// @desc    Login logic with authorization JWT
+// @route   POST /auth/login
+// @access  Public
 exports.loginUser = async (req, res) => {
-    // Login logic goes here
-    const { email, password } = req.body;
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
     const user = await User.findOne({ where: { email } });
 
-    if (!user || !user.validPassword(password)) {
-        console.log({ message: 'Invalid email or password' });
-        return res.status(401).json({ message: 'Invalid email or password' });
+    if (!user) {
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { title: "incorrect email or password" },
+      });
     }
 
+    // Compaire hash(login pass) to hash (db user password hash)
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { title: "incorrect email or password" },
+      });
+    }
+
+    //   const token = createToken(userId: user.id, firstName: user.firstName, lastName: user.lastName ); //Bara code
+    //   const refreshToken= createRefreshToken(userId: user.id, firstName: user.firstName, lastName: user.lastName )
     // User is authenticated, issue a token
-    const token = jwt.sign({ userId: user.id, firstName: user.firstName, lastName: user.lastName }, config.auth.accessToken, { expiresIn: '7d'/*'15m'*/ });
+    const token = jwt.sign(
+      { userId: user.id, firstName: user.firstName, lastName: user.lastName },
+      config.auth.accessToken,
+      { expiresIn: "7d" /*'15m'*/ }
+    );
 
-    const refreshTokenValue = jwt.sign({ userId: user.id, firstName: user.firstName, lastName: user.lastName }, config.auth.refreshToken, { expiresIn: '7d' });
+    const refreshTokenValue = jwt.sign(
+      { userId: user.id, firstName: user.firstName, lastName: user.lastName },
+      config.auth.refreshToken,
+      { expiresIn: "7d" }
+    );
 
-    /* const refreshToken = await RefreshToken.create({
+    // Send the token to the client
+    return res.json({
+      status: httpStatusText.SUCCESS,
+      data: { user: user, token },
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ status: httpStatusText.ERROR, data: { title: error.message } });
+  }
+  /* const refreshToken = await RefreshToken.create({
          token: refreshTokenValue,
          userId: user.id // assuming user is your Sequelize user instance
      });*/
-
-    console.log({ token });
-    // Send the token to the client
-    return res.json({ token });
 };
 
+// @desc    forget pass logic,send verification code to email
+// @route   POST /auth/forgot-password
+// @access  Public
+exports.forgotPassword =async (req, res) => {
+  //1) Get user by email
+  const email = req.body.email;
+  const user = await User.findByPk(email);
+  if (!user) {
+    return res.json({
+      status: httpStatusText.FAIL,
+      data: { title: "there is no user with that email", email: email },
+    });
+  }
 
-exports.forgotPassword = (req, res) => {
-    // Here you should implement your process for password recovery.
-    // Typically, this involves sending an email to the user with a unique link
-    // they can use to reset their password.
-    res.status(501).json({ error: 'Not implemented' });
+  //2) If user exist Generate reset random 6 digits and save in db
+  const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
+  const hashedResetCode = crypto
+    .createHash("sha256")
+    .update(resetCode)
+    .digest("hex");
+
+  // Save hashedResetCode into db
+  user.passwordResetCode = hashedResetCode;
+
+  // Add expiration time for reset code (10 min)
+  user.passwordResetExpire = Date.now() + 10 * 60 * 1000;
+  user.passwordResetVerified = false;
+  await user.save(); // await to save the user changes
+
+  //3) Send the reset code via email
+  // const content=`Hi ${user.username}, We received a request to reset the password `
+  const text = `<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Password Reset</title>
+</head>
+<body>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
+        <h2>Password Reset Request</h2>
+        <p>Hello, ${user.username}</p>
+        <p>We have received a request to reset your password. Your password reset code is:</p>
+        <p style="background-color: #f4f4f4; padding: 10px; font-size: 20px;">${resetCode}</p>
+        <p>If you didn't request a password reset, please ignore this email or contact our support team.</p>
+        <p>Thank you!</p>
+    </div>
+</body>
+</html>`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset code (valid for 10 min)",
+      content: text,
+    });
+  } catch (error) {
+    user.passwordResetCode = null;
+    user.passwordResetExpire = null;
+    user.passwordResetVerified = null;
+    await user.save();
+
+    return res.json({
+      status: httpStatusText.ERROR,
+      data: { title: "email not sent", message: error.message },
+    });
+  }
+
+  return res.json({
+    status: httpStatusText.SUCCESS,
+    data: { title: "Reset code sent to your email. Check your inbox." },
+  });
 };
 
-exports.resetPassword = (req, res) => {
-    // Here you should implement your process for resetting a user's password.
-    // This usually involves checking a token sent in the reset password request
-    // against one stored in the database or the reset password link,
-    // then updating the user's password in the database if the tokens match.
-    res.status(501).json({ error: 'Not implemented' });
+// @desc    Verify the code sent to your email
+// @route   POST /api/verifyPasswordResetCode
+// @access  Public
+exports.verifyPasswordResetCode = async (req, res, next) => {
+    // 1) Get user based on reset code
+    const hashedResetCode = crypto
+      .createHash("sha256")
+      .update(req.body.resetCode)
+      .digest("hex");
+  
+    const user = await User.findOne({
+      where: {
+        passwordResetCode: hashedResetCode,
+        passwordResetExpire: {
+          [Op.gt]: new Date(), // Check if the reset expiration is in the future
+        },
+      },
+    });
+    if (!user) {
+      return res.json({
+        status: httpStatusText.FAIL,
+        data: { title: "reset code invalid" },
+      });
+    }
+  
+    // 2) Reset code valid
+  
+    user.passwordResetVerified = true;
+    await user.save();
+    return res.json({
+      status: httpStatusText.SUCCESS,
+      data: { title: "reset code verified" },
+    });
+  };
+
+// @desc    Reset password - 3ed step in forget passwrod process
+// @route   PUT /auth/reset-password
+// @access  Public
+exports.resetPassword = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  //********Validation********
+  //check if the password and confirm password is matched or not
+  if (password !== confirmPassword) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "password and confirmation password must be the same" },
+    });
+  }
+
+  //check password length
+  if (password.length < 8 || confirmPassword.length < 8) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: {
+        title: "password length should be equal or more than 8 characters",
+      },
+    });
+  }
+
+  //check if the user email not valid or not valid (email syntax check)
+  const isValid = await validator.isEmail(email);
+  if (!isValid) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "you entered invalid email" },
+    });
+  }
+  //***************************
+
+  // 1) Get user based on email
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      status: httpStatusText.FAIL,
+      data: { title: "there is no user with this email" },
+    });
+  }
+
+  // 2) check if reset code verified
+  if (!user.passwordResetVerified) {
+    return res.status(400).json({
+      status: httpStatusText.FAIL,
+      data: { title: "reset code not verified" },
+    });
+  }
+  // Reset the password
+  try {
+    // - Encrypt the new password
+
+    const encryptPassword = await bcrypt.hash(req.body.password, 12);
+    user.password = encryptPassword;
+    user.passwordResetCode = null;
+    user.passwordResetExpire = null;
+    user.passwordResetVerified = null;
+    await user.save();
+
+    // 3) everything is ok
+    return res.json({
+      status: httpStatusText.SUCCESS,
+      data: { title: "Password changed successfully" },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: httpStatusText.ERROR,
+      data: { title: "Something went wrong, please try again." },
+    });
+  }
 };
