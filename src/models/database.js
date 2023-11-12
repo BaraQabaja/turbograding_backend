ca7c273fdb9b81db1e7586459b7a50a3fb442619
BaraQabaja <bara.qabaja@gmail.com>
2023-11-12 10:37:58 +0200

    heroku commit

--- src/models/database.js
+++ src/models/database.js
@@ -1,21 +1,33 @@
 const config = require('../config');
 
 const Sequelize = require('sequelize');
-console.log( config.db.database,
-    config.db.username,
-    config.db.password,)
-const sequelize = new Sequelize(
-    config.db.database,
-    config.db.username,
-    config.db.password,
-    {
-        host: config.db.host,
-        dialect: 'postgres',
-        port: 5432, //should be remove
-        // dialectOptions: {
-        //     ssl: { rejectUnauthorized: false }
-        // } //should be remove
-    });
+
+// const sequelize = new Sequelize(
+//     config.db.database,
+//     config.db.username,
+//     config.db.password,
+//     {
+//         host: config.db.host,
+//         dialect: 'postgres',
+//         port: 5432, //should be remove
+//         dialectOptions: {
+//             ssl: { rejectUnauthorized: false }
+//         } //should be remove
+//     });
+
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< This is important
+      },
+    },
+    // other configurations...
+  });
+  
 
 module.exports = sequelize;
 
