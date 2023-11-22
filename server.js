45d5b1c5c96020412ccd1196001be110fef03b03
BaraQubaja <bqubaja@turbograding.com>
2023-11-22 11:35:15 +0200

    heroku commit

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log("cors origin ===> ",origin)
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
