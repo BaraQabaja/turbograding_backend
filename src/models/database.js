68d6a71e455d678b4041477d63fae8ab4a382995
BaraQubaja <bqubaja@turbograding.com>
2023-12-03 18:10:08 +0200

    heroku commit

--- src/models/database.js
+++ src/models/database.js
@@ -27,7 +27,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       },
     },
     pool: {
-      max: 100, // Adjust as needed
+      max: 20, // Adjust as needed
       min: 0,
       acquire: 30000,
       idle: 10000,
