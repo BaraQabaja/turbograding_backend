e81e18eb5abf940ab6898ce7bcfed841fbabe5be
BaraQubaja <bqubaja@turbograding.com>
2023-12-03 10:18:21 +0200

    heroku commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,26 +22,49 @@ exports.gradingExam = async (req, res) => {
 
   try {
     const info = req.body;
+    const {
+      course_code,
+      course_name,
+      university_name,
+      semester,
+      class_code,
+      studentId,
+      studentFirstName,
+      studentLastName,
+    } = info;
     // 1) Create Course
-    const course = await Course.create({
-      course_code: info.course_code,
-      course_name: info.course_name,
+    //check if the course exist or not
+    const course = await Course.findOne({
+      where: { course_code: course_code },
     });
+    // if not exist create the course
+    if (!course) {
+      await Course.create({
+        course_code: course_code,
+        course_name: course_name,
+      });
+    }
 
     // 2) Create University
-    const university = await University.create({
-      university_name: info.university_name,
+    //check if the university exist or not
+    const university = await University.findOne({
+      where: { university_name: university_name },
     });
-
+    // if not exist create the university
+    if (!university) {
+      await University.create({
+        university_name: university_name,
+      });
+    }
     // 3) Create userUniversity
     // - get university_name to access the university id
     const university_intity = await University.findOne({
       where: {
-        university_name: info.university_name,
+        university_name: university_name,
       },
     });
     if (!university_intity) {
-        console.log("fail finding university");
+      console.log("fail finding university");
 
       return res.status(400).json({
         status: httpStatusText.FAIL,
@@ -50,21 +73,28 @@ exports.gradingExam = async (req, res) => {
         },
       });
     }
-    // - create the userUniversity
-    const userUniversity = await UserUniversity.create({
-      universityId: university_intity.id,
-      UserId: req.user.id,
+    // check if the userUniversity is exist or not, if the professor is working at this university or not
+    const userUniversity = await UserUniversity.findOne({
+      where: { universityId: university_intity.id, UserId: req.user.id },
     });
+
+    if (!userUniversity) {
+      // - create the userUniversity
+      await UserUniversity.create({
+        universityId: university_intity.id,
+        UserId: req.user.id,
+      });
+    }
 
     // 4) Create CourseOffering
     // - get the course id we want to offer
     const course_intity = await Course.findOne({
       where: {
-        course_code: info.course_code,
+        course_code: course_code,
       },
     });
     if (!course_intity) {
-        console.log("fail finding course");
+      console.log("fail finding course");
 
       return res.status(400).json({
         status: httpStatusText.FAIL,
@@ -73,32 +103,59 @@ exports.gradingExam = async (req, res) => {
         },
       });
     }
-    // - create the courseOffering
-    const courseOffering = await CourseOffering.create({
-      semester_name: info.semester,
-      class_code: info.class_code,
-      courseId: course_intity.id,
-      UserId: req.user.id,
+
+    // check if the course offered or not
+    const courseOffering = await CourseOffering.findOne({
+      where: {
+        semester_name: semester,
+        class_code: class_code,
+        courseId: course_intity.id,
+        UserId: req.user.id,
+      },
     });
+    if (!courseOffering) {
+      // - create the courseOffering
+      await CourseOffering.create({
+        semester_name: semester,
+        class_code: class_code,
+        courseId: course_intity.id,
+        UserId: req.user.id,
+      });
+    }
+
     // 5) Create Student
-    const student = await Student.create({
-      id: info.studentId,
-      first_name: info.studentFirstName,
-      last_name: info.studentLastName,
-      universityId: university_intity.id,
+    // check if the student exist or not
+    const student = await Student.findOne({
+      where: {
+        id: info.studentId,
+        first_name: studentFirstName,
+        last_name: studentLastName,
+        universityId: university_intity.id,
+      },
     });
-    // 6) Create Enrollment for this student on specific course
+    if (!student) {
+      // create student
+      await Student.create({
+        id: info.studentId,
+        first_name: studentFirstName,
+        last_name: studentLastName,
+        universityId: university_intity.id,
+      });
+    }
+    // 6) Create Enrollment for this student on specific course(offered course)
     // - get the courseOffering id (the id of the offeredCourse we just created in step 4)
+
     const courseOffering_intity = await CourseOffering.findOne({
-      where: {//we match all these attributes because every attribute can be changed while other attributes still fixed
-        semester_name: info.semester_name,
-        class_code: info.class_code,
+      where: {
+        //we match all these attributes because every attribute can be changed while other attributes still fixed
+        semester_name: semester,
+        class_code: class_code,
         courseId: course_intity.id,
         UserId: req.user.id,
       },
     });
     if (!courseOffering_intity) {
-        console.log("fail finding courseOffering");
+      console.log("fail finding courseOffering");
       return res.status(400).json({
         status: httpStatusText.FAIL,
         data: {
@@ -106,11 +163,22 @@ exports.gradingExam = async (req, res) => {
         },
       });
     }
-    // - create the courseOffering
-    const enrollment = await Enrollment.create({
-     courseOfferingId:courseOffering_intity.id,
-     studentId:info.studentId
+    // check if the student enrolled in the courseoffering or not
+    const enrollment = await Enrollment.findOne({
+      where: {
+        courseOfferingId: courseOffering_intity.id,
+        studentId: studentId,
+      },
     });
+
+    if (!enrollment) {
+      // - create the courseOffering
+      await Enrollment.create({
+        courseOfferingId: courseOffering_intity.id,
+        studentId: studentId,
+      });
+    }
+
     // // 7) Create Exam
     // const exam = await Exam.create({
     //   id: info.studentId,
@@ -133,7 +201,7 @@ exports.gradingExam = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log("error happend in gradingExam ==> ", error.message);
+    console.log("error happend in gradingExam ==> ", error);
     return res.status(500).json({
       status: httpStatusText.ERROR,
       data: {
@@ -142,3 +210,20 @@ exports.gradingExam = async (req, res) => {
     });
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
