f1b7df8e2c788565702412bf76cd18fc57b23056
BaraQubaja <bqubaja@turbograding.com>
2023-12-06 19:12:01 +0200

    heroku commit

--- src/routes/user.js
+++ src/routes/user.js
@@ -25,7 +25,6 @@ router.get('/get-user-universities',auth.protect, userController.getUserUniversi
 router.get('/get-user-courses',auth.protect, userController.getUserCourses);
 router.get('/get-semesters',auth.protect, userController.getSemesters);
 router.get('/get-user-classes',auth.protect, userController.getUserClasses);
-router.get('/get-class-exams',auth.protect, userController.getClassExams);
 router.get('/students-exam-info',auth.protect, userController.getStudentsExamInfo);
 // router.post('/assignment', authenticateToken, upload.single('file'), usersController.gradingAssignment);
 
