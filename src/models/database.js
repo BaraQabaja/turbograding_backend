6929617dbf01a89c0239063fb3572cea1d24d5ec
BaraQubaja <bqubaja@turbograding.com>
2023-12-07 15:09:57 +0200

    heroku commit

--- src/models/database.js
+++ src/models/database.js
@@ -1,26 +1,7 @@
-const config = require("../config");
+const config = require('../config');
 
-const Sequelize = require("sequelize");
-// ... Other imports
-const {
-  UserModal,
-  UniversityModal,
-  UserUniversityModal,
-  CourseModal,
-  ActivityModal,
-  SemesterModal,
-  ClassModal,
-  LocationModal,
-  StudentModal,
-  CourseOfferingModal,
-  UserCourseOfferingModal,
-  EnrollmentModal,
-  GradeModal,
-  ExamModal,
-  PaymentModal,
-  PlanModal,
-  SubscriptionModal,
-} = require("./index");
+const Sequelize = require('sequelize');
+
 //! on Development
 // const sequelize = new Sequelize(
 //     config.db.database,
@@ -36,64 +17,24 @@ const {
 //     });
 
 //! on production
-const sequelizeInstance = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // <<<<<<< This is important
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< This is important
+      },
     },
-  },
-  pool: {
-    max: 20, // Adjust as needed
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  // other configurations...
-});
-//! Modals
-const UserModal = require("./User");
-const UniversityModal = require("./University");
-const UserUniversityModal = require("./UserUniversity");
-const CourseModal = require("./Course");
-const ActivityModal = require("./Activity");
-// const CourseOfferingSemesterModal = require("./src/models/CourseOfferingSemester");
-const SemesterModal = require("./Semester");
-const ClassModal = require("./Class_Info");
-const LocationModal = require("./Location_Info");
+    pool: {
+      max: 20, // Adjust as needed
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    // other configurations...
+  });
 
-const StudentModal = require("./Student");
-const CourseOfferingModal = require("./CourseOffering");
-const UserCourseOfferingModal = require("./UserCourseOffering");
 
-const EnrollmentModal = require("./Enrollment");
+module.exports = sequelize;
 
-const GradeModal = require("./Grade");
-const ExamModal = require("./Exam");
-
-const PaymentModal = require("./Payment");
-const PlanModal = require("./Plan");
-const SubscriptionModal = require("./Subscription");
-
-module.exports = {
-  sequelize: sequelizeInstance,
-  UserModal,
-  UniversityModal,
-  UserUniversityModal,
-  CourseModal,
-  ActivityModal,
-  SemesterModal,
-  ClassModal,
-  LocationModal,
-  StudentModal,
-  CourseOfferingModal,
-  UserCourseOfferingModal,
-  EnrollmentModal,
-  GradeModal,
-  ExamModal,
-  PaymentModal,
-  PlanModal,
-  SubscriptionModal,
-};
