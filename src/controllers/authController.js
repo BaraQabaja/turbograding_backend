d5064d8852e4afeadea30b06a3c66c30da316c50
BaraQubaja <bqubaja@turbograding.com>
2023-12-11 09:58:27 +0200

    heroku commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -86,7 +86,6 @@ exports.protect = async (req, res, next) => {
 };
 
 exports.emailVerification = async (req, res) => {
-
   const token = req.params.token;
   const user = await User.findOne({ where: { emailVerificationToken: token } });
   if (!user) {
@@ -226,57 +225,74 @@ exports.registerUser = async (req, res) => {
                 // );
                 const verificationLink = `http://localhost:3000/verify/${token}`; //! for now keep it like this, when you want to deploy the app you should change it to be   const verificationLink = `${req.protocole}://${req.get('host')}/verify/${token}`;
 
+                // Create basic subscription for the user
+                // 1) Find plan - from function parameters
+                const plan = await Plan.findOne({
+                  where: { name: "basic" },
+                });
+                if (!plan) {
+                  return res.status(404).json({
+                    status: httpStatusText.FAIL,
+                    data: { title: "something went wrong, please try again." },
+                  });
+                }
+                const planId = plan.id;
+                // 2) Create sub
+                const subscriptionEnd_ms = 99999999999 * 1000;
+                //convert from seconds to milliseconds
+                const subscriptionStart_ms = 100 * 1000;
+                //convert from seconds to milliseconds
+                try {
+                  const subscription = await Subscription.create({
+                    userId: user.id,
+                    planId: planId,
+                    startDate: new Date(subscriptionStart_ms),
+                    endDate: new Date(subscriptionEnd_ms),
+                    status: "active",
+                    remainingQuestions: plan.questions,
+                    remainingExams: plan.exams,
+                    remainingAssignments: plan.assignments,
+                  });
+
+                  console.log(
+                    "subscription created successfully.",
+                    subscription
+                  );
+                } catch (error) {
+                  console.log("regestration error");
+
+                  console.log(error.message);
+                  return res.status(500).json({
+                    status: httpStatusText.FAIL,
+                    data: { title: "registration failed" },
+                  });
+                }
+                // 2) send email
                 await sendEmail({
                   email: user.email,
                   subject: "Account Verification",
                   content: `Click <a href="${verificationLink}">here</a> to verify your account.`,
                 });
               } catch (error) {
-                console.log("error in user regesteration ====> ",error.message)
+                console.log(
+                  "error in user regesteration ====> ",
+                  error.message
+                );
                 user.emailVerificationToken = null;
 
                 await user.save();
 
                 return res.json({
                   status: httpStatusText.ERROR,
-                  data: { title: "email not sent", message: error.message },
-                });
-              }
-              // Create basic subscription for the user
-              // 1) Find plan - from function parameters
-              const plan = await Plan.findOne({
-                where: { name: "basic" },
-              });
-              const planId = plan.id;
-              // 2) Create sub
-              const subscriptionEnd_ms = 99999999999 * 1000;
-              //convert from seconds to milliseconds
-              const subscriptionStart_ms = 100 * 1000;
-              //convert from seconds to milliseconds
-              try {
-                const subscription = await Subscription.create({
-                  userId: user.id,
-                  planId: planId,
-                  startDate: new Date(subscriptionStart_ms),
-                  endDate: new Date(subscriptionEnd_ms),
-                  status: "active",
-                  remainingQuestions: plan.questions,
-                  remainingExams: plan.exams,
-                  remainingAssignments: plan.assignments,
-                });
-
-                console.log("subscription created successfully.", subscription);
-              } catch (error) {
-                console.log("regestration error");
-
-                console.log(error.message);
-                return res.status(500).json({
-                  status: httpStatusText.FAIL,
-                  data: { title: "registration failed" },
+                  data: {
+                    title:
+                      "email not sent, something went wrong please try again.",
+                    message: error.message,
+                  },
                 });
               }
 
-              return res.status(200).json({
+              return res.json({
                 status: httpStatusText.SUCCESS,
                 data: {
                   title:
@@ -292,21 +308,30 @@ exports.registerUser = async (req, res) => {
             } else {
               return res.status(400).json({
                 status: httpStatusText.FAIL,
-                data: { title: "registration failed" },
+                data: { title: "Registration failed, please try again" },
               });
             }
           })
-          .catch((err) => {
-            return res
-              .status(500)
-              .send(err || "Something went wrong in the db");
+          .catch((error) => {
+            console.log("Something went wrong in the db ===> ", error);
+            return res.status(500).json({
+              status: httpStatusText.FAIL,
+              data: { title: "Something went wrong, please try again." },
+            });
           });
       } else {
-        return res.send("something went wrong in returning hash password");
+        return res.json({
+          status: httpStatusText.FAIL,
+          data: { title: "Something went wrong, please try again." },
+        });
       }
     })
-    .catch((err) => {
-      return res.status(500).send(err || "Something went wrong");
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({
+        status: httpStatusText.FAIL,
+        data: { title: "Something went wrong, please try again." },
+      });
     });
 };
 
@@ -348,13 +373,13 @@ exports.loginUser = async (req, res) => {
         data: { title: "Invalid email or password" },
       });
     }
- // Check if email is verified
- if (!user.verifiedEmail) {
-  return res.status(401).json({
-    status: httpStatusText.FAIL,
-    data: { title: "Please verify your email before logging in" },
-  });
-}
+    // Check if email is verified
+    if (!user.verifiedEmail) {
+      return res.status(401).json({
+        status: httpStatusText.FAIL,
+        data: { title: "Please verify your email before logging in" },
+      });
+    }
     //   const token = createToken(userId: user.id, firstName: user.firstName, lastName: user.lastName ); //Bara code
     //   const refreshToken= createRefreshToken(userId: user.id, firstName: user.firstName, lastName: user.lastName )
     // User is authenticated, issue a token
@@ -392,15 +417,17 @@ exports.loginUser = async (req, res) => {
 // @access  Public
 exports.forgotPassword = async (req, res) => {
   //1) Get user by email
- 
+
   const email = req.body.email;
-  const user = await User.findOne({where:{
-    email:email
-  }});
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
   if (!user) {
     return res.json({
       status: httpStatusText.FAIL,
-      data: { title: "there is no user with that email", email:email },
+      data: { title: "there is no user with that email", email: email },
     });
   }
 
@@ -412,14 +439,13 @@ exports.forgotPassword = async (req, res) => {
     .digest("hex");
 
   // Save hashedResetCode into db
-  console.log("generated reset code ===> ",hashedResetCode)
+  console.log("generated reset code ===> ", hashedResetCode);
   user.passwordResetCode = hashedResetCode;
 
   // Add expiration time for reset code (10 min)
   user.passwordResetExpire = Date.now() + 10 * 60 * 1000;
   user.passwordResetVerified = false;
   await user.save(); // await to save the user changes
-
 
   //3) Send the reset code via email
   // const content=`Hi ${user.username}, We received a request to reset the password `
@@ -440,80 +466,76 @@ exports.forgotPassword = async (req, res) => {
     </div>
 </body>
 </html>`;
-  console.log("email sent ..........")
+  console.log("email sent ..........");
 
-  try{
+  try {
     await sendEmail({
       email: user.email,
       subject: "Your password reset code (valid for 10 min)",
       content: text,
     });
-   
-    
+
     return res.json({
       status: httpStatusText.SUCCESS,
       data: { title: "Reset code sent to your email. Check your inbox." },
     });
-  }catch(error){
+  } catch (error) {
     user.passwordResetCode = null;
     user.passwordResetExpire = null;
     user.passwordResetVerified = null;
     await user.save();
 
-      console.log("error in forget pass ===> ",error.message)
-      return res.json({
-        status: httpStatusText.ERROR,
-        data: { title: "Email not sent"},
-      });
-    }
-  
-
-  
+    console.log("error in forget pass ===> ", error.message);
+    return res.json({
+      status: httpStatusText.ERROR,
+      data: { title: "Email not sent" },
+    });
+  }
 };
 
 // @desc    Verify the code sent to your email - 2nd step in forget password process
 // @route   POST /api/verifyPasswordResetCode
 // @access  Public
 exports.verifyPasswordResetCode = async (req, res, next) => {
-  try{
-console.log('i am in verifyPasswordResetCode',req.body.resetCode)
-  
-  // 1) Get user based on reset code
-  const hashedResetCode = crypto
-    .createHash("sha256")
-    .update(req.body.resetCode)
-    .digest("hex");
+  try {
+    console.log("i am in verifyPasswordResetCode", req.body.resetCode);
 
-  const user = await User.findOne({
-    where: {
-      passwordResetCode: hashedResetCode,
-      passwordResetExpire: {
-        [Op.gt]: new Date(), // Check if the reset expiration is in the future
+    // 1) Get user based on reset code
+    const hashedResetCode = crypto
+      .createHash("sha256")
+      .update(req.body.resetCode)
+      .digest("hex");
+
+    const user = await User.findOne({
+      where: {
+        passwordResetCode: hashedResetCode,
+        passwordResetExpire: {
+          [Op.gt]: new Date(), // Check if the reset expiration is in the future
+        },
       },
-    },
-  });
-  if (!user) {
-    return res.json({
-      status: httpStatusText.FAIL,
-      data: { title: "reset code invalid" },
     });
-  }
+    if (!user) {
+      return res.json({
+        status: httpStatusText.FAIL,
+        data: { title: "reset code invalid" },
+      });
+    }
 
-  // 2) Reset code valid
+    // 2) Reset code valid
 
-  user.passwordResetVerified = true;
-  await user.save();
-  return res.json({
-    status: httpStatusText.SUCCESS,
-    data: { title: "reset code verified" },
-  });
-}catch(error){
-    console.log("error in verifyPasswordResetCode ====> ",error.message)
+    user.passwordResetVerified = true;
+    await user.save();
+    return res.json({
+      status: httpStatusText.SUCCESS,
+      data: { title: "reset code verified" },
+    });
+  } catch (error) {
+    console.log("error in verifyPasswordResetCode ====> ", error.message);
     return res.status(500).json({
       status: httpStatusText.ERROR,
       data: { title: "error in verify Password Reset Code" },
     });
-}
+  }
 };
 
 // @desc    Reset password - 3ed step in forget passwrod process
@@ -521,61 +543,61 @@ console.log('i am in verifyPasswordResetCode',req.body.resetCode)
 // @access  Public
 exports.resetPassword = async (req, res) => {
   try {
-  const email = req.body.email;
-  const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  //********Validation********
-  //check if the password and confirm password is matched or not
-  if (password !== confirmPassword) {
-    return res.status(404).json({
-      status: httpStatusText.FAIL,
-      data: { title: "password and confirmation password must be the same" },
-    });
-  }
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    //********Validation********
+    //check if the password and confirm password is matched or not
+    if (password !== confirmPassword) {
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { title: "password and confirmation password must be the same" },
+      });
+    }
 
-  //check password length
-  if (password.length < 8 || confirmPassword.length < 8) {
-    return res.status(404).json({
-      status: httpStatusText.FAIL,
-      data: {
-        title: "password length should be equal or more than 8 characters",
+    //check password length
+    if (password.length < 8 || confirmPassword.length < 8) {
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: {
+          title: "password length should be equal or more than 8 characters",
+        },
+      });
+    }
+
+    //check if the user email not valid or not valid (email syntax check) - prev sql injection
+    const isValid = await validator.isEmail(email);
+    if (!isValid) {
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { title: "you entered invalid email" },
+      });
+    }
+    //***************************
+
+    // 1) Get user based on email
+    const user = await User.findOne({
+      where: {
+        email: email,
       },
     });
-  }
 
-  //check if the user email not valid or not valid (email syntax check) - prev sql injection
-  const isValid = await validator.isEmail(email);
-  if (!isValid) {
-    return res.status(404).json({
-      status: httpStatusText.FAIL,
-      data: { title: "you entered invalid email" },
-    });
-  }
-  //***************************
+    if (!user) {
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { title: "there is no user with this email" },
+      });
+    }
 
-  // 1) Get user based on email
-  const user = await User.findOne({
-    where: {
-      email: email,
-    },
-  });
+    // 2) check if reset code verified
+    if (!user.passwordResetVerified) {
+      return res.status(400).json({
+        status: httpStatusText.FAIL,
+        data: { title: "reset code not verified" },
+      });
+    }
+    // Reset the password
 
-  if (!user) {
-    return res.status(404).json({
-      status: httpStatusText.FAIL,
-      data: { title: "there is no user with this email" },
-    });
-  }
-
-  // 2) check if reset code verified
-  if (!user.passwordResetVerified) {
-    return res.status(400).json({
-      status: httpStatusText.FAIL,
-      data: { title: "reset code not verified" },
-    });
-  }
-  // Reset the password
-  
     // - Encrypt the new password
 
     const encryptPassword = await bcrypt.hash(req.body.password, 12);
