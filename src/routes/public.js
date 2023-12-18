198dc7c442658a25d059cb5f9af1fcbeed25ceea
BaraQubaja <bqubaja@turbograding.com>
2023-12-18 11:11:29 +0200

    heroku commit

--- src/routes/public.js
+++ src/routes/public.js
@@ -3,6 +3,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.put("/get-all-plans", planController.getPlans); // get all plans
+router.get("/get-all-plans", planController.getPlans); // get all plans
 
 module.exports = router;
